676e1e0a1739ce01e73f48ebbdb19ffc41d43577
Hasselmann0 <isaacbilac920@gmail.com>
2025-09-05 16:21:45 -0300

    ui atualizado

--- frontEnd - UI/src/app/usuario/usuario.ts
+++ frontEnd - UI/src/app/usuario/usuario.ts
@@ -1,19 +1,42 @@
-import { Component } from '@angular/core';
-import { UsuarioService } from '../services/usuario.service';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
 import { UsuarioInterface } from '../intefaces/usuario.interface';
-import { AsyncPipe } from '@angular/common';
+import { UsuarioService } from '../services/usuario.service';
+
 
 @Component({
     selector: 'app-usuario',
-    imports: [MatTableModule, AsyncPipe],
     templateUrl: './usuario.html',
-    styleUrl: './usuario.css'
+    styleUrl: './usuario.css',
+    imports: [
+        CommonModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+    ],
 })
-export class Usuario {
+export class Usuario implements AfterViewInit {
 
-//dataSource = new MatTableDataSource<UsuarioService>();
+    dataSource = new MatTableDataSource<UsuarioInterface>([]);
+    displayedColumns: string[] = ['id', 'nomeDoDevedor', 'nomeDoEstabelecimento', 'valorTotalDaCompra',
+        'diaDaCompra', 'status'];
+
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
 
     usuarios$ = new Observable<UsuarioInterface[]>();
 
@@ -24,6 +47,14 @@ export class Usuario {
 
     RetornarTodosUsuarios() {
         this.usuarios$ = this.usuarioService.RetornarTodos();
+        this.usuarios$.pipe(takeUntilDestroyed()).subscribe(users => {
+            this.dataSource.data = users ?? [];
+        })
     }
+    
+    ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    } 
 }  
 
